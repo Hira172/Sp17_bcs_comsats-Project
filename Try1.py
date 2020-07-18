abb583556395ec61dd58f11b139f42f35ff3255a
Hira Muazzam <hiramuazzam172@gmail.com>
2020-07-19 00:21:40 +0500

    Run file

--- Try1.py
+++ Try1.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 # Load Yolo
-net = cv2.dnn.readNet("D:\\fyp\Codes\yolov3.weights", "yolov3.cfg")
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
